3d125927cf7ba1f391f6665d9b5bf7561d1a6b95
Dylan Scott <dylanrhysscott@gmail.com>
2023-03-07 00:14:51 +0000

    Fix missing package

--- src/terraform/install.sh
+++ src/terraform/install.sh
@@ -2,7 +2,7 @@
 set -e
 TERRAFORM_VERSION="1.3.9"
 apt-get update 
-apt-get install -y zip
+apt-get install -y zip curl
 curl -LO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 chmod +x terraform
