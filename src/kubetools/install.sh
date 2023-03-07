b6e1db398c781dbda1c9a8021767646a1bbb7fb7
Dylan Scott <dylanrhysscott@gmail.com>
2023-03-07 00:34:07 +0000

    Add kubetools features

--- src/kubetools/install.sh
+++ src/kubetools/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -1
 KUBETOOLS_VERSION="v0.9.4"
+apt-get update 
+apt-get install -y zip curl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO https://github.com/ahmetb/kubectx/releases/download/${KUBETOOLS_VERSION}/kubectx_${KUBETOOLS_VERSION}_linux_x86_64.tar.gz
 curl -LO https://github.com/ahmetb/kubectx/releases/download/${KUBETOOLS_VERSION}/kubens_${KUBETOOLS_VERSION}_linux_x86_64.tar.gz
